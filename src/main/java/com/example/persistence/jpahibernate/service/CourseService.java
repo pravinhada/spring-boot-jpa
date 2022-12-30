fa5881962eb1eb26b9f4f509c2efd5bd10547aaf
Prabin Hada <pravin_hada@yahoo.com>
2022-12-30 11:03:01 -0500

    refactor

--- src/main/java/com/example/persistence/jpahibernate/service/CourseService.java
+++ src/main/java/com/example/persistence/jpahibernate/service/CourseService.java
@@ -2,36 +2,21 @@ package com.example.persistence.jpahibernate.service;
 
 import com.example.persistence.jpahibernate.dto.CourseDto;
 import com.example.persistence.jpahibernate.model.Course;
-import com.example.persistence.jpahibernate.model.Student;
 import com.example.persistence.jpahibernate.repo.CourseRepository;
-import com.example.persistence.jpahibernate.repo.StudentRepository;
 import com.example.persistence.jpahibernate.request.CourseRequest;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CourseService {
 
     final private CourseRepository courseRepository;
-    final private StudentRepository studentRepository;
-
-    @Transactional
-    public void removeStudent() {
-        Course course = this.courseRepository.findById(1L)
-                .orElseThrow(() -> new IllegalArgumentException("Course with this id is not found."));
-        Student student = this.studentRepository.findById(8L).orElseThrow(
-                () -> new IllegalArgumentException("Student with this id is not found."));
-        // course.removeStudent(student);
-        log.info("student is removed ", student);
-    }
 
     @Transactional(readOnly = true)
     public Course getCourse(Long id) {
